cc6bec5d727759805504ab75013623d19638cf44
Tristan Angelo Babasa <tristan.babasa@novare.com.hk>
2016-10-08 23:01:22 +0800

    finshied working demo

--- code/www/js/app.js
+++ code/www/js/app.js
@@ -30,13 +30,24 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
   $stateProvider
 
 
-  // Set up an abstract state for the tabs directive:
-  .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html',
-    controller: 'TabsCtrl'
-  })
+  // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html',
+        controller: 'TabsCtrl',
+        // don't load the state until we've populated our User, if necessary.
+        resolve: {
+          populateSession: function(User) {
+            return User.checkSession();
+          }
+        },
+        onEnter: function($state, User){
+          User.checkSession().then(function(hasSession) {
+            if (!hasSession) $state.go('splash');
+          });
+        }
+      })
 
   // Each tab has its own nav history stack:
 
@@ -59,8 +70,19 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
         }
       }
     })
+  // splash page
+      .state('splash', {
+        url: '/',
+        templateUrl: 'templates/splash.html',
+        controller: 'SplashCtrl',
+        onEnter: function($state, User){
+          User.checkSession().then(function(hasSession) {
+            if (hasSession) $state.go('tab.discover');
+          });
+        }
+      })
   // If none of the above states are matched, use this as the fallback:
-  $urlRouterProvider.otherwise('/tab/discover');
+  $urlRouterProvider.otherwise('/');
 
 })
 
--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,22 +4,134 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope) {
+.controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations, $ionicLoading) {
 
+    // helper functions for loading
+    var showLoading = function() {
+        $ionicLoading.show({
+            template: '<i class="ion-loading-c"></i>',
+            noBackdrop: true
+        });
+    }
+
+    var hideLoading = function() {
+        $ionicLoading.hide();
+    }
+
+    // set loading to true first time while we retrieve songs from server.
+    showLoading();
+    
+    // get our first songs
+    Recommendations.init()
+        .then(function(){
+            $scope.currentSong = Recommendations.queue[0];
+            Recommendations.playCurrentSong();
+            hideLoading();
+        })
+        .then(function(){
+            // turn loading off
+            hideLoading();
+            $scope.currentSong.loaded = true;
+        });
+
+    // fired when we favorite / skip a song.
+    $scope.sendFeedback = function (bool) {
+
+        // first, add to favorites if they favorited
+        console.log($scope.currentSong.title);
+        if (bool) User.addSongToFavorites($scope.currentSong);
+
+        // set variable for the correct animation sequence
+        $scope.currentSong.rated = bool;
+        $scope.currentSong.hide = true;
+
+        // prepare the next song
+        Recommendations.nextSong();
+
+        // update current song in scope, timeout to allow animation to complete
+        $timeout(function() {
+            $scope.currentSong = Recommendations.queue[0];
+            $scope.currentSong.loaded = false;
+        }, 250);
+
+        Recommendations.playCurrentSong().then(function() {
+            $scope.currentSong.loaded = true;
+        });
+
+    }
+
+    // used for retrieving the next album image.
+    // if there isn't an album image available next, return empty string.
+    $scope.nextAlbumImg = function() {
+        if (Recommendations.queue.length > 1) {
+            return Recommendations.queue[1].image_large;
+        }
+
+        return '';
+    }
 })
 
 
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope) {
+    .controller('FavoritesCtrl', function($scope, User, $window) {
+        // get the list of our favorites from the user service
+        $scope.username = User.username;
+        $scope.favorites = User.favorites;
+        
+        console.log('hey ' + User.favorites.title)
 
-})
+        $scope.removeSong = function(song, index) {
+            User.removeSongFromFavorites(song, index);
+        }
 
+        $scope.openSong = function(song) {
+            $window.open(song.open_url, "_system");
+        }
+    })
+        
+        
 
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+    .controller('TabsCtrl', function($scope, Recommendations, User, $window) {
 
-});
+        $scope.favCount = User.favoriteCount;
+        // stop audio when going to favorites page
+        $scope.enteringFavorites = function() {
+            User.newFavorites = 0;
+            Recommendations.haltAudio();
+        }
+
+        $scope.leavingFavorites = function() {
+            Recommendations.init();
+        }
+
+        $scope.logout = function() {
+            User.destroySession();
+
+            // instead of using $state.go, we're going to redirect.
+            // reason: we need to ensure views aren't cached.
+            $window.location.href = 'index.html';
+        }
+
+    })
+
+    .controller('SplashCtrl', function($scope, $state, User) {
+
+        // attempt to signup/login via User.auth
+        $scope.submitForm = function(username, signingUp) {
+            User.auth(username, signingUp).then(function(){
+                // session is now set, so lets redirect to discover page
+                $state.go('tab.discover');
+
+            }, function() {
+                // error handling here
+                alert('Hmm... try another username.');
+
+            });
+        }
+
+    });
